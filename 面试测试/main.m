40870b6fff4a111dea039f961890027bfbd57e63
吴宝锋 <wubaofeng@hudunsoft.com>
2021-07-12 15:08:02 +0800

    Initial Commit

---
+++ 面试测试/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  面试测试
+//
+//  Created by zy on 2021/7/12.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
